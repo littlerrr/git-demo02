cb2fef6a85ba6d2ab874dbb1b13977b0240978ff
hh <1604867366@qq.com>
2022-11-14 22:28:38 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,5 +3,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
