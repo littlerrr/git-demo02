96be22f07a10b02b0621a3730f288be55d6f392b
hh <1604867366@qq.com>
2023-03-23 14:43:25 +0800

    测试提交过程

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -1,13 +1,8 @@
 package com.git;
 
+
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World2");
-        System.out.println("Hello World3");
-        System.out.println("Hello World4");
-        System.out.println("hot-mix commit");
-        System.out.println("master commit1");
-        System.out.println("pull text");
+        System.out.println("test阶段");
     }
 }
